a62db18dbafa5f5216e53d964566b5d1631f11ea
iDigitalFlame <idf@idigitalflame.com>
2019-07-25 19:54:27 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 
+OUT="scoreboard"
+if [ $# -ge 1 ]; then
+    OUT="$1"
+fi
+
 echo "Building..."
 packr2
-go build -o scorebard cmd/scoreboard/main.go
+go build -o "$OUT" cmd/scoreboard/main.go
 packr2 clean
 echo "Done!"
